10e287e7a09a9fb98e7d94598139be8dd1b85e20
JosephMina23 <josephmesias2000@gmail.com>
2024-11-28 10:39:19 -0500

    Fix Migracion/Modelo

--- app/Models/Misvis.php
+++ app/Models/Misvis.php
@@ -5,7 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Mis-vis extends Model
+class Misvis extends Model
 {
     use HasFactory;
 }
