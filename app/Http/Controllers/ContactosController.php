2687ae76a9073c094e910ca568dd6da6f322f128
Beth1998 <tatyliz1998@gmail.com>
2024-11-28 12:39:44 -0500

    controller

--- app/Http/Controllers/ContactosController.php
+++ app/Http/Controllers/ContactosController.php
@@ -8,4 +8,20 @@ use App\Model\Contactos;
 class ContactosController extends Controller
 {
     //
+    public function Contactos(){ //Mostrar info
+        $getContactos = Contactos::all();
+        return view('/home', compact('getContactos'));
+    }
+
+    public function EditC($id){ //llamar a la vista
+        $editContactos = Contactos::where('id', $id)->firstOrfail();
+        return view('/editarC', compact('editContactos'));
+    }
+
+    public function UpdateC(Request $request, $id){
+        $updateC = Contactos::findOrFail($id);
+        $requestC = $request->all();
+        $updateC->update($requestC);
+        return redirect()->to('/home');
+    }
 }
--- app/Http/Controllers/ServiciosController.php
+++ app/Http/Controllers/ServiciosController.php
@@ -8,4 +8,21 @@ use App\Model\Servicios;
 class ServiciosController extends Controller
 {
     //
+
+    public function Servicios(){
+        $getServicios = Servicios::all();
+        return view('/home', compact('getServicios'));
+    }
+
+    public function EditSV($id){
+        $editServicios = Servicios::where('id', $id)->firstOrfail();
+        return view('/editarSV', compact('editServicios'));
+    }
+
+    public function UpdateSV(Request $request, $id){
+        $updateSV = Servicios::findOrfail($id);
+        $requestSV = $request->all();
+        $updateSV->update($requestSV);
+        return redirect()->to('/home');
+    }
 }
